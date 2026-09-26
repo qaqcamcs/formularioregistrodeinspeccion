21b79a25d154604201e2caf00fdf921fb837edeb
qaqcamcs <matias.cortez.f@gmail.com>
2026-09-26 10:07:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: guarda la app en el dispositivo para abrirla sin internet.
 // Si cambias index.html o los iconos, sube el número de versión para forzar la actualización.
-const CACHE = 'qaqc-v5';
+const CACHE = 'qaqc-v7';
 const SHELL = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png', './icons/apple-touch-icon.png'];
 
